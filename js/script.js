0cc1837f805d731af1f83a0ecbf201aaa6ec09f0
Urszula Patyra <upatyra@wp.pl>
2021-11-29 00:26:28 +0100

    Change articles selector

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 'use strict';
+
 /*document.getElementById('test-button').addEventListener('click', function(){
     const links = document.querySelectorAll('.titles a');
     console.log('links:', links);
   });*/
   const titleClickHandler = function(event){
-    console.log('Link was clicked!');
+        console.log('Link was clicked!');
+        console.log(event)
   
-    /* remove class 'active' from all article links  */
+    /*[DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
 for(let activeLink of activeLinks){
@@ -14,10 +16,10 @@ for(let activeLink of activeLinks){
 }
 
   
-    /* add class 'active' to the clicked link */
+    /*[IN PROGRESS] add class 'active' to the clicked link */
   
-    /* remove class 'active' from all articles */
-    const activeArticles = document.querySelectorAll('.post .active');
+    /* [DONE]remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.posts a.active');
 
 for(let activeArticles of activeArticles){
   activeArticles.classList.remove('active');
